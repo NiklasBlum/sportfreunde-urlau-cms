b881608159d75ba6d9c3417808a24fccae348669
Niklas Blum <niklas.blum97@gmail.com>
2026-03-15 10:32:04 +0100

    added appId

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,13 +3,14 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'n07z17nt',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
+    appId: 'x0cpl8n6041g24winoo958bf',
     autoUpdates: true,
-  }
+  },
 })
